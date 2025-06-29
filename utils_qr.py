17e4c5b8763a00a48f1ce843e6868214d835f4b7
Alexis García <agitechnology33@gmail.com>
2025-06-28 22:55:36 -0600

    Agrega función QR y enlace en inventario para mostrar código QR

--- utils_qr.py
+++ utils_qr.py
@@ -1,5 +1,8 @@
 import qrcode
+import os
 
-def generar_qr(texto, destino_io):
-    img = qrcode.make(texto)
-    img.save(destino_io, format="PNG")
+def generar_qr(texto, ruta_salida):
+    os.makedirs(os.path.dirname(ruta_salida), exist_ok=True)
+    qr = qrcode.make(texto)
+    qr.save(ruta_salida)
+
